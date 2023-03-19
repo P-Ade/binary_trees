37df23d9b1ef7a3cc5acf8d99b93d0f00fb8d919
P-Ade <109440726+P-Ade@users.noreply.github.com>
2023-03-19 20:35:43 +0100

    Create 111-bst_insert.c

---
+++ 111-bst_insert.c
@@ -0,0 +1,28 @@
+		{
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
+		}
+		if (value < current->n)
+		{
+			if (current->left != NULL)
+				return (bst_insert(&current->left, value));
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (current->left = new);
+		}
+		if (value > current->n)
+		{
+			if (current->right != NULL)
+				return (bst_insert(&current->right, value));
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (current->right = new);
+		}
+	}
+
+	return (NULL);
+}
